159d74ac3656e12d6492bcb67d1c4f6df0e8ac45
sergeyivanchik <ivanchik-s@mail.ru>
2020-12-17 02:46:58 +0300

    fix

---
+++ src/components/App/index.jsx
@@ -0,0 +1,11 @@
+import './index.scss'
+
+function App() {
+  return (
+    <div className="app">
+      <div style={{fontSize: '2rem'}}>Hello World!</div>
+    </div>
+  )
+}
+
+export default App
---
+++ src/styles/images/index.js
@@ -0,0 +1,4 @@
+import logoHeader from './logo-header.png'
+
+
+export const LOGO_HEADER = logoHeader
