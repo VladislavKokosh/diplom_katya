922d91b418b448984369683e242a86228c326a80
Vladislav Kokosh <iamkoksss@mail.ru>
2020-12-18 15:32:21 +0300

    purchase conditions

--- src/components/Purchase/index.jsx
+++ src/components/Purchase/index.jsx
@@ -1,9 +1,8 @@
 import React from 'react'
 
-import Slider from './Slider'
-
 import './index.scss'
 
+import { PURCHASE } from '../../styles/images'
 import { items } from './items'
 
 
@@ -15,17 +14,29 @@ const Purchase = () => {
         <div className='purchase__wrapper'>
           <div className ='purchase__text'>
             <span className ='purchase__text-title'>ОАО «Березовский комбикормовый завод» закупает:</span>
-            <ul>
+            <ul className ='purchase__text-ul'>
               {
                 items.map((item, index) => {
-                  return <li>{item}</li>
+                  return <li key={index}>{item}</li>
                 })
               }
             </ul>
-            Условия поставки: железнодорожным или автотранспортом на условиях ФСО и ФСН.
-            Объем поставок: по согласованию сторон.
-            Порядок расчетов: денежными средствами, возможен взаиморасчет встречной поставкой продукции. 
-            Закупочная цена: индивидуальное рассмотрение предложений в зависимости от объемов и условий поставки.
+          </div>
+          <div className ='purchase__conditions'>
+            <span className ='purchase__conditions-title'>Условия</span>
+            <img
+                src={PURCHASE}
+                alt='logo'
+                className='purchase__conditions-image'
+              /> 
+            <div className ='purchase__conditions-content'>
+              <div className ='purchase__conditions-content-text'>
+                <b>Условия поставки:</b> железнодорожным или автотранспортом на условиях ФСО и ФСН.<br/>
+                <b>Объем поставок:</b> по согласованию сторон.<br/>
+                <b>Порядок расчетов:</b> денежными средствами, возможен взаиморасчет встречной поставкой продукции.<br/>
+                <b>Закупочная цена:</b> индивидуальное рассмотрение предложений в зависимости от объемов и условий поставки.
+              </div>
+            </div>
           </div>
         </div>
       </div>
--- src/styles/images/index.js
+++ src/styles/images/index.js
@@ -1,9 +1,5 @@
 import logoHeader from './logo-header1.png'
-import pshenica from './pshenica_furajnaya.jpg'
-import triracale from './tritacale_furajnaya.jpg'
-import yachmen from './yachmen-furazhnyj.jpg'
+import purchase from './purchase.png'
 
 export const LOGO_HEADER = logoHeader
-export const PSHENICA = pshenica
-export const TRIRACALE = triracale
-export const YACHMEN = yachmen
+export const PURCHASE = purchase
