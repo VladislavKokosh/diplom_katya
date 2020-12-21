8252321f7ca4e3fb811e159d2e5b29fe6e82adb4
Sergey Ivanchik <ivanchik-s@mail.ru>
2020-12-21 16:40:50 +0300

    fix

--- src/components/Contacts/WorkersContacts/index.jsx
+++ src/components/Contacts/WorkersContacts/index.jsx
@@ -7,16 +7,20 @@ const WorkersContacts = ({ contacts }) => {
       {
         contacts.map((item, index) => (
           <div className='contacts__container_workers-item' key={index}>
-            <div className='contacts__container_workers-item-position'>{item.position}</div>
-            <div className='contacts__container_workers-item-name'>{item.name}</div>
-            <a 
-              className='contacts__container_workers-item-phone' 
+            <div className='contacts__container_workers-item-position'>
+              {item.position}
+            </div>
+            <div className='contacts__container_workers-item-name'>
+              {item.name}
+            </div>
+            <a
+              className='contacts__container_workers-item-phone'
               href={'tel:'+item.phone}
             >
                 {item.phone}
             </a>
-            <a 
-              className='contacts__container_workers-item-mail' 
+            <a
+              className='contacts__container_workers-item-mail'
               href={'mailto:'+item.mail}
             >
                 {item.mail}
