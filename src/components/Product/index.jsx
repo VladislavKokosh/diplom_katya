112b397dfa24e255516f615bf02bdf78b7edd83e
Vladislav Kokosh <iamkoksss@mail.ru>
2021-02-04 16:17:16 +0300

    production component, need fix

--- src/components/Product/index.jsx
+++ src/components/Product/index.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 
+import Table from '../Table'
 import './index.scss'
 
 import { items } from './items'
 
 
 const Product = ({ product }) => {
+  const [active, setActive] = useState(0)
+  console.log(items[product].grade[active].tableOne);
   return (
     <div className='product'>
       <div className='product__container'>
@@ -14,6 +17,21 @@ const Product = ({ product }) => {
             <img src={items[product].img} alt='' className='product__img-image'/>
           </div>
           <div className ='product__title'>ОАО «Березовский комбикормовый  завод предлагает:</div>
+          <div className='product__grade'>
+            {items[product].grade.map((item, index) => (
+              <span 
+                className={active === index ? 'product__grade-item-active' : 'product__grade-item'}
+                key={index}
+                onClick={() => setActive(index)}
+              >
+                {item.name}
+              </span>
+            ))}
+          </div>
+          <div>{items[product].grade[active].infoOne}</div>
+          <Table title='123' content={items[product].grade[active].tableOne}></Table>
+          <div>{items[product].grade[active].infoTwo}</div>
+          <Table title='123' content={items[product].grade[active].tableTwo}></Table>
           <div className ='product__name'>{items[product].name}</div>
           <div className='product__info'>
             {items[product].info}
--- src/components/Table/index.jsx
+++ src/components/Table/index.jsx
@@ -3,12 +3,12 @@ import React from 'react'
 const Table = ({title, content}) => {
   return(
     <table>
-      <caption>{title}</caption>
+      <caption>{title ? title : '123'}</caption>
         {
           content.map((item, index) => (
             <tr key={index}>
-              <td>{item.сolumnOne}</td>
-              <td>{item.columnTwo}</td>
+              <td>{item?.сolumnOne}</td>
+              <td>{item?.columnTwo}</td>
               {item.columnTree && <td>{item.columnTree}</td>}
             </tr>
           ))
