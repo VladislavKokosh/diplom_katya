499e0752310438756ac9eb86a26f44895558ffe1
iamkoks <iamkoksss@mail.ru>
2021-02-17 22:11:56 +0300

    adaptability for product, purchase and export, contacts need fix

--- src/components/Contacts/Map/index.jsx
+++ src/components/Contacts/Map/index.jsx
@@ -5,7 +5,7 @@ import { YMaps, Map, Placemark } from 'react-yandex-maps'
 const MyMap = () => {
 	return(
 		<YMaps>
-				<Map defaultState={{ center: [52.530297, 24.907256], zoom: 17 }} width={600} height={600}>
+				<Map defaultState={{ center: [52.530297, 24.907256], zoom: 17 }} width={'100%'} height={600}>
 					<Placemark geometry={[52.530297, 24.907256]} />
 				</Map>
 		</YMaps>
